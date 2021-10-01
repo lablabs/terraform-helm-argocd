dfbdf94b453f268ec8e5a8c4d6f4302486fa2504
Martin Hauskrecht <hauskrechtmartin@gmail.com>
2021-10-01 18:42:55 +0200

    Cleanup

--- application.tf
+++ application.tf
@@ -1,38 +1,3 @@
-# resource "kubernetes_manifest" "self" {
-#   count = var.enabled && var.self_managed ? 1 : 0
-#   manifest = {
-#     "apiVersion" = "argoproj.io/v1alpha1"
-#     "kind"       = "Application"
-#     "metadata" = {
-#       "name"      = var.helm_release_name
-#       "namespace" = var.k8s_namespace
-#     }
-#     "spec" = {
-#       "project" = var.argo_project
-#       "source" = {
-#         "repoURL"        = var.helm_repo_url
-#         "chart"          = var.helm_chart_name
-#         "targetRevision" = var.helm_chart_version
-#         "helm" = {
-#           "releaseName" = var.helm_release_name
-#           "parameters"  = [for k, v in var.settings : tomap({ "forceString" : true, "name" : k, "value" : v })]
-#           "values"      = var.values
-#         }
-#       }
-#       "destination" = {
-#         "server"    = var.argo_destionation_server
-#         "namespace" = var.k8s_namespace
-#       }
-#       "syncPolicy" = var.argo_sync_policy
-#       "info"       = var.argo_info
-#     }
-#   }
-
-#   depends_on = [
-#     helm_release.self_managed[0]
-#   ]
-# }
-
 locals {
   argo_application_values = yamlencode({
     "project" : var.argo_project
@@ -62,7 +27,6 @@ data "utils_deep_merge_yaml" "argo_application_values" {
     var.argo_application_values
   ])
 }
-
 
 resource "helm_release" "argocd_application" {
   count = var.enabled && var.self_managed ? 1 : 0
