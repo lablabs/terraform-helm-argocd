743ccff3b93f13afb8e2b0431f6cdb79ae07a8c4
Martin Hauskrecht <hauskrechtmartin@gmail.com>
2021-10-05 13:57:55 +0200

    Use same values for both application deployment approaches

--- application.tf
+++ application.tf
@@ -54,25 +54,7 @@ resource "kubernetes_manifest" "self" {
       "name"      = var.helm_release_name
       "namespace" = var.k8s_namespace
     }
-    "spec" = {
-      "project" = var.argo_project
-      "source" = {
-        "repoURL"        = var.helm_repo_url
-        "chart"          = var.helm_chart_name
-        "targetRevision" = var.helm_chart_version
-        "helm" = {
-          "releaseName" = var.helm_release_name
-          "parameters"  = [for k, v in var.settings : tomap({ "forceString" : true, "name" : k, "value" : v })]
-          "values"      = var.values
-        }
-      }
-      "destination" = {
-        "server"    = var.argo_destionation_server
-        "namespace" = var.k8s_namespace
-      }
-      "syncPolicy" = var.argo_sync_policy
-      "info"       = var.argo_info
-    }
+    "spec" = local.argo_application_values
   }
 
   depends_on = [
