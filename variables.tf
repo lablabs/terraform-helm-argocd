10d5c39008770d034acdb3d02a5068e0eea1806f
tomas-balaz <tomasbalaz3012@gmail.com>
2022-08-23 18:40:37 +0200

    feat: update helm_chart_version

--- variables.tf
+++ variables.tf
@@ -18,7 +18,7 @@ variable "helm_chart_name" {
 
 variable "helm_chart_version" {
   type        = string
-  default     = "3.17.5"
+  default     = "4.10.8"
   description = "Version of the Helm chart"
 }
 
