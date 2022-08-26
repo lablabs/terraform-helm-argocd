e6a86426970537d5638d1148e65b40aae43a3ed6
tomas-balaz <tomasbalaz3012@gmail.com>
2022-08-26 04:55:36 +0200

    fix: remove aws provider

--- versions.tf
+++ versions.tf
@@ -2,10 +2,6 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.19.0"
-    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 2.11.0"
